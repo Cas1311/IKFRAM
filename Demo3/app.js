b7381c6e646ded1133bd8d9059a4938c89ab103b
Cas1311 <86728367+Cas1311@users.noreply.github.com>
2025-05-22 17:34:42 +0200

    demo 3

---
+++ Demo3/app.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+    };
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    detract(num) {
+      this.counter = this.counter - num;
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    submitForm() {
+      alert('Submitted!');
+    },
+  },
+});
+
+app.mount('#events');
